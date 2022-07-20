d161f45c03ed32b1be9035cda87ab9740267fb1b
zishan344 <zishanahmed344@gmail.com>
2022-07-20 18:21:46 +0600

    done the firebase section implement

---
+++ src/Component/src/Dashboard.js
@@ -0,0 +1,47 @@
+import { signOut } from "firebase/auth";
+import React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { VscThreeBars } from "react-icons/vsc";
+import { NavLink, Outlet } from "react-router-dom";
+import auth from "../../firebase.init";
+const Dashboard = () => {
+  const [user, loading, error] = useAuthState(auth);
+  return (
+    <div className="drawer drawer-mobile">
+      <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col items-center justify-center">
+        {/* <!-- Page content here --> */}
+        <Outlet />
+
+        <label
+          htmlFor="my-drawer-2"
+          className="btn btn-secondary btn-outline drawer-button absolute top-0 right-3 lg:hidden"
+        >
+          <VscThreeBars />
+        </label>
+      </div>
+      <div className="drawer-side">
+        <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+        <ul className="menu p-4 overflow-y-auto w-60 md:w-80 bg-base-100 text-base-content">
+          {/* <!-- Sidebar content here --> */}
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          {user ? (
+            <li>
+              <NavLink onClick={() => signOut(auth)} to="/login">
+                Logout
+              </NavLink>
+            </li>
+          ) : (
+            <li>
+              <NavLink to="/login">Login</NavLink>
+            </li>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/Component/src/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Welcome to Dashboard Panel</h2>
+    </div>
+  );
+};
+
+export default Home;
